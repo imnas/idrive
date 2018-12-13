b4ae09b6606f29afbcca7aab4704c239d2fbd437
Dennis Onder <webdev.dennisonder@gmail.com>
2018-12-13 21:26:58 +0100

    Added edit route for profiles and added a simple checking system for profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,22 +34,65 @@ router.get('/current', passport.authenticate('jwt', { session: false }), (req, r
 // @ACCESS  - Private
 // @DESC    - Add a new profile
 router.post('/add', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Check user type
   if (req.body.type === 'instructor') {
-    const newProfile = new InstructorProfile({
-      // Add experience and similar meta data
-      user: req.body.user,
-      proof: req.body.proof,
-      profilePicture: req.body.profilePicture
-    });
-    newProfile.save();
-    res.status(200).json(newProfile);
+    // Check if a profile exists
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        const newProfile = new InstructorProfile({
+          // Add experience and similar meta data
+          user: req.body.user,
+          proof: req.body.proof,
+          profilePicture: req.body.profilePicture
+        });
+        newProfile.save();
+        res.status(200).json(newProfile);
+      } else {
+        res.status(401).send('You already have a profile.');
+      }
+    })
   } else if (req.body.type === 'learner') {
-    const newProfile = new LearnerProfile({
-      profilePicture: req.body.profilePicture
-    });
-    newProfile.save();
-    res.status(200).json(newProfile);
+    // Check if a profile exists
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        const newProfile = new LearnerProfile({
+          profilePicture: req.body.profilePicture
+        });
+        newProfile.save();
+        res.status(200).json(newProfile);
+      } else {
+        res.status(401).send('You already have a profile.');
+      }
+    })
   }
 });
+
+// @PATH    - PUT /api/profile/edit
+// @ACCESS  - Private
+// @DESC    - Edit a profile
+router.put('/edit', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Check user type
+  if (req.body.type === 'instructor') {
+    InstructorProfile.findOneAndUpdate({ user: req.body.id })
+    .then(profile => {
+      if(profile) {
+        // Edit the profile here
+      } else {
+        res.status(404).send('Profile not found.');
+      };
+    });
+  } else if (req.body.type === 'learner') {
+    LearnerProfile.findOneAndUpdate({ user: req.body.id })
+    .then(profile => {
+      if(profile) {
+        // Edit the profile here
+      } else {
+        res.status(404).send('Profile not found.');
+      };
+    });
+  }
+})
 
 module.exports = router;
