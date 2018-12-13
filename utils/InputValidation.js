403672e859f5ff0fa850dca362e8406c74d53e53
Dennis Onder <webdev.dennisonder@gmail.com>
2018-12-13 21:47:46 +0100

    Added form validation object which will contain multiple functions for form validation purposes

---
+++ utils/InputValidation.js
@@ -0,0 +1,43 @@
+// Add validation functions to the exported object below.
+const Validator = require('validator');
+
+module.exports = {
+  validateRegistrationForm(data) {
+    let errors = {};
+    if(Validator.isEmpty(data.firstName)) {
+      errors.firstNameEmpty = 'First Name Field Is Required.';
+    }
+    if(Validator.isEmpty(data.lastName)) {
+      errors.lastNameEmpty = 'Last Name Field Is Required.';
+    }
+    if(Validator.isEmpty(data.password)) {
+      errors.passwordEmpty = 'Password Field Is Required.';
+    }
+    if(data.password !== data.confirmPassword) {
+      errors.passwordNotMatching = 'Passwords Are Not Matching.';
+    }
+    if(!Validator.isLength(data.password, { min: 8, max: 32 })) {
+      errors.passwordLength = 'Password Length Should Be Between 8 And 32 Characters.';
+    }
+    if(Validator.isEmpty(data.email)) {
+      errors.emailEmpty = 'Email Field Is Required.';
+    }
+    if(!Validator.isEmail(data.email)) {
+      errors.emailInvalid = 'Email Address Is Not Valid.';
+    }
+    if(Validator.isEmpty(data.phone)) {
+      errors.phoneEmpty = 'Phone Field Is Required.';
+    }
+    if(Validator.isEmpty(data.city)) {
+      errors.cityEmpty = 'City Name Field Is Required.';
+    }
+    if(Validator.isEmpty(data.postalCode)) {
+      errors.postalCodeEmpty = 'Postal Code Field Is Required.';
+    }
+    if(errors.firstNameEmpty || errors.lastNameEmpty || errors.passwordEmpty || errors.passwordLength || errors.passwordNotMatching || errors.emailEmpty || errors.emailInvalid || errors.phoneEmpty || errors.cityEmpty || errors.postalCodeEmpty) {
+      return errors;
+    } else {
+      return true;
+    }
+  },
+}
