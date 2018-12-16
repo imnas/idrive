63e2225e1967c27e4b18a8031c92a548332a794d
Dennis Onder <webdev.dennisonder@gmail.com>
2018-12-16 01:36:59 +0100

    GitHub Script

--- autocommit.sh
+++ autocommit.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# AutoCommit to GitHub
-
-git add .
-read -p "Commit description: " desc 
-git commit -m "$desc"
-git push
-
-echo 'You have succesfully pushed to GitHub!'
+read -p "Commit Message: " desc
+git add . && \
+git add -u && \
+git commit -m "$desc" && \
+git push origin master
