0140f969c24c05200a45217d14a1ec7a099f26ef
DennisOnder <webdev.dennisonder@gmail.com>
2019-01-18 23:44:59 +0100

    Refactored the lookup endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,86 +6,86 @@ const Learner = require('../../models/Learner');
 const LearnerProfile = require('../../models/LearnerProfile');
 const passport = require('passport');
 
-// @PATH    - GET /api/:type/:id
+// @PATH    - GET /api/users/:handle
 // @ACCESS  - Private
 // @DESC    - Get user by ID
-router.get('/:type/:id', passport.authenticate('jwt', {
+router.get('/:handle', passport.authenticate('jwt', {
     session: false
 }), (req, res) => {
-    if (req.params.type === 'instructor') {
-        Instructor.findById(req.params.id)
-            .then(user => {
-                if (user) {
-                    const instructor = {
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        email: user.email,
-                        phone: user.phone,
-                        city: user.city,
-                        postalCode: user.postalCode,
-                        address: user.address
-                    };
-                    return res.status(200).json(instructor);
-                } else {
-                    return res.status(404).send('Instructor not found.');
-                }
-            })
-    } else if (req.params.type = 'learner') {
-        Learner.findById(req.params.id)
-            .then(user => {
-                if (user) {
-                    const learner = {
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        email: user.email,
-                        phone: user.phone,
-                        city: user.city,
-                        postalCode: user.postalCode,
-                        address: user.address
-                    };
-                    return res.status(200).json(learner);
-                } else {
-                    return res.status(404).send('Learner not found.');
-                }
-            })
-    }
+    Instructor.findOne({
+            handle: req.params.handle
+        })
+        .then(user => {
+            if (user) {
+                const instructor = {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    phone: user.phone,
+                    city: user.city,
+                    postalCode: user.postalCode,
+                    address: user.address
+                };
+                return res.status(200).json(instructor);
+            } else {
+                Learner.findOne({
+                        handle: req.params.handle
+                    })
+                    .then(user => {
+                        if (user) {
+                            const learner = {
+                                firstName: user.firstName,
+                                lastName: user.lastName,
+                                email: user.email,
+                                phone: user.phone,
+                                city: user.city,
+                                postalCode: user.postalCode,
+                                address: user.address
+                            };
+                            return res.status(200).json(learner);
+                        } else {
+                            return res.status(404).send('User not found.');
+                        }
+                    })
+            }
+        })
 });
 
-// @PATH    - GET /api/:type/:id/current
+// @PATH    - GET /api/users/:handle/profile
 // @ACCESS  - Private
 // @DESC    - Get user by ID
-router.get('/:type/:id/current', passport.authenticate('jwt', {
+router.get('/:handle/profile', passport.authenticate('jwt', {
     session: false
 }), (req, res) => {
-    if (req.params.type === 'instructor') {
-        InstructorProfile.findById(req.params.id)
-            .then(profile => {
-                if (profile) {
-                    const instructor = {
-                        profilePicture: profile.profilePicture,
-                        biography: profile.biography,
-                        experience: profile.experience,
-                        created: profile.created
-                    };
-                    return res.status(200).json(instructor);
-                } else {
-                    return res.status(404).send('Profile not found.');
-                }
-            })
-    } else if (req.params.type = 'learner') {
-        LearnerProfile.findById(req.params.id)
-            .then(profile => {
-                if (profile) {
-                    const learner = {
-                        profilePicture: profile.profilePicture,
-                        created: profile.created
-                    };
-                    return res.status(200).json(learner);
-                } else {
-                    return res.status(404).send('Profile not found.');
-                }
-            })
-    }
+    InstructorProfile.findOne({
+            handle: req.params.handle
+        })
+        .then(profile => {
+            if (profile) {
+                const instructor = {
+                    profilePicture: profile.profilePicture,
+                    biography: profile.biography,
+                    experience: profile.experience,
+                    created: profile.created
+                };
+                return res.status(200).json(instructor);
+            } else {
+                LearnerProfile.findOne({
+                        handle: req.params.handle
+                    })
+                    .then(profile => {
+                        if (profile) {
+                            const learner = {
+                                profilePicture: profile.profilePicture,
+                                created: profile.created
+                            };
+                            return res.status(200).json(learner);
+                        } else {
+                            return res.status(404).send('Profile not found.');
+                        }
+                    })
+            }
+        })
 });
 
 module.exports = router;
