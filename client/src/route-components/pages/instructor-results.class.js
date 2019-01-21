5a32bcde456b5f57e0f87e6e59215a88b1308040
jaymontana <nasakthe@gmail.com>
2019-01-21 22:42:08 +0000

    Search filter top

--- client/src/route-components/pages/instructor-results.class.js
+++ client/src/route-components/pages/instructor-results.class.js
@@ -1,13 +1,65 @@
 import React, { Component } from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+const distance = ["+ 1 miles", "+ 30 miles", "+ 40 miles"];
+const transmission = ["Both", "Automatic", "Manual"];
+const gender = ["Any", "Male", "Female"];
+
+const defaultOptionDistance = distance[0];
+const defaultOptionTransmission = transmission[0];
+const defaultOptionGender = gender[0];
 
 export class InstructorResults extends Component {
   render() {
     return (
       <div className="resultsWrapper">
-        <div className="resultsFilterWrapper">
+        <div className="resultsFilterTopWrap">
           <div className="resultsFilterContainer">
             <h4>Driving instructors near SK17 2JE</h4>
+            <div className="filterOptionsContainer">
+              <div className="filterRow">
+                <div className="individualFilterContainer">
+                  <label>Your Location</label>
+                  <input value="SK17 2JE" />
+                </div>
+                <div className="individualFilterContainer">
+                  <label>Search Distance</label>
+                  <Dropdown
+                    options={distance}
+                    onChange={this._onSelect}
+                    value={defaultOptionDistance}
+                    placeholder="+ 1 miles"
+                  />
+                </div>
+                <div className="individualFilterContainer">
+                  <label>Transmission Type</label>
+                  <Dropdown
+                    options={transmission}
+                    onChange={this._onSelect}
+                    value={defaultOptionTransmission}
+                    placeholder="Automatic"
+                  />
+                </div>
+                <div className="individualFilterContainer">
+                  <label>Instructor Gender</label>
+                  <Dropdown
+                    options={gender}
+                    onChange={this._onSelect}
+                    value={defaultOptionGender}
+                    placeholder="Any"
+                  />
+                </div>
+                <div className="searchCtaContainerResults">
+                  <button type="submit">
+                    Update <i class="fas fa-search" />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="resultsFilterWrapper">
           <div className="resultsContainer">
             <div>xxx</div>
           </div>
