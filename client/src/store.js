409246815fe9549fec3bd2ad3eaadbdf6669e7ff
DennisOnder <webdev.dennisonder@gmail.com>
2019-01-22 23:30:15 +0100

    Edited the store file

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 export default function configureStore(initialState = {}) {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer, initialState, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 }
