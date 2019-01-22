2b053abe8bc472ff385250805a538a90ade7d462
DennisOnder <webdev.dennisonder@gmail.com>
2019-01-22 23:31:08 +0100

    Comments

--- client/src/store.js
+++ client/src/store.js
@@ -2,5 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 export default function configureStore(initialState = {}) {
+  // TODO: Remove the devtools extension code for production
   return createStore(rootReducer, initialState, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 }
