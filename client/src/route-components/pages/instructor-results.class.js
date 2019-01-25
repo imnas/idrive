3c051906292ac846fa1caa0a6e309b690f2344d9
jaymontana <nasakthe@gmail.com>
2019-01-25 17:27:36 +0000

    remove old display results

--- client/src/route-components/pages/instructor-results.class.js
+++ client/src/route-components/pages/instructor-results.class.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getInstructors } from '../../actions/resultActions';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getInstructors } from "../../actions/resultActions";
 
 const distance = ["+ 1 miles", "+ 30 miles", "+ 40 miles"];
 const transmission = ["Both", "Automatic", "Manual"];
@@ -94,62 +94,6 @@ export class InstructorResults extends Component {
         </div>
         <div className="resultsFilterWrapper">
           <div className="resultsContainer">
-            {/* {this.state.TEMP_DATA.map((instructor, index) => {
-              return (
-                <div
-                  key={index}
-                  className="individualResult"
-                  style={{ marginBottom: "1em" }}
-                >
-                  <div className="iRTop">
-                    <div
-                      className="profileCarContainer"
-                      style={{
-                        backgroundImage:
-                          "url(" +
-                          "https://mydrivinglesson.co.uk/wp-content/uploads/2018/02/gp-pro-pic1-220x220.jpg" +
-                          ")"
-                      }}
-                    />
-                    <div className="instructorDetailsContainer">
-                      <h4>{instructor.name}</h4>
-                      <p>Driving Instructor</p>
-                      <div className="skillsContainer">
-                        <span>ADI CoP</span>
-                        <span>CPD</span>
-                        <span>Multilingual</span>
-                      </div>
-                      <div className="vehicleContainer">
-                        <h5>Vehicle</h5>
-                        <div className="skillsContainer">
-                          <span>Automatic</span>
-                          <span>Petrol</span>
-                          <span>Saloon</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="locationDistance">0.3 Miles away</div>
-                  </div>
-                  <div className="irBot">
-                    <div className="instructorCta">
-                      <a>Book Now</a>
-                    </div>
-                    <div className="abilitiesContainer">
-                      <h4>
-                        <i class="fas fa-hand-holding-magic" /> £10/Lesson
-                      </h4>
-                      <h4>
-                        <i class="fas fa-trophy-alt" /> 100% Success Rate
-                      </h4>
-                      <h4>
-                        <i class="fas fa-car" /> Porsche Panamera
-                      </h4>
-                    </div>
-                  </div>
-                </div>
-              );
-            })} */}
-
             {this.props.results.instructors.map((instructor, index) => {
               return (
                 <div key={index} className="individualResultAlt">
@@ -215,11 +159,14 @@ InstructorResults.propTypes = {
   getInstructors: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
   results: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   errors: state.errors,
   results: state.results
 });
 
-export default connect(mapStateToProps, { getInstructors })(InstructorResults);
+export default connect(
+  mapStateToProps,
+  { getInstructors }
+)(InstructorResults);
