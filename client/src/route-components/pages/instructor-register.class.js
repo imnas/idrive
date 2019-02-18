71fa828e09e5ea85bee01b618bae9ca082d62cf4
jaymontana <nasakthe@gmail.com>
2019-02-18 23:48:07 +0000

    image crop

--- client/src/route-components/pages/instructor-register.class.js
+++ client/src/route-components/pages/instructor-register.class.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import Checkbox from "rc-checkbox";
 import { RadioGroup, RadioButton } from "react-radio-buttons";
-import Header from "../includes/header.class";
 import { NavLink } from "react-router-dom";
+import Header from "../includes/header.class";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 import "rc-checkbox/assets/index.css";
 
 export default class InstructorRegister extends Component {
   constructor() {
     super();
-    this.state = { gender: "" };
+    this.state = {
+      gender: "",
+      src: null,
+      imagesrc1: null,
+      imagesrc2: null,
+      imagesrc3: null,
+      crop: {
+        aspect: 1,
+        width: 50
+      }
+    };
   }
 
   componentDidMount = () => {
-    document.title = 'InstantDriving - Profile'; 
-  }
+    document.title = "InstantDriving - Profile";
+  };
+
+  confirmUpload = () => {
+    if (this.state.croppedImageUrl) {
+      if (!this.state.imagesrc1) {
+        this.setState({
+          imagesrc1: this.state.croppedImageUrl
+        });
+      } else if (!this.state.imagesrc2) {
+        this.setState({
+          imagesrc2: this.state.croppedImageUrl
+        });
+      } else {
+        this.setState({
+          imagesrc3: this.state.croppedImageUrl
+        });
+      }
+    }
+  };
 
   gender = e => {
     if (e.target.checked) {
@@ -27,12 +57,79 @@ export default class InstructorRegister extends Component {
     }
   };
 
+  onSelectFile = e => {
+    if (e.target.files && e.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () =>
+        this.setState({ src: reader.result })
+      );
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  onImageLoaded = (image, pixelCrop) => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = (crop, pixelCrop) => {
+    this.makeClientCrop(crop, pixelCrop);
+  };
+
+  onCropChange = crop => {
+    this.setState({ crop });
+  };
+
+  async makeClientCrop(crop, pixelCrop) {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageUrl = await this.getCroppedImg(
+        this.imageRef,
+        pixelCrop,
+        "newFile.jpeg"
+      );
+      this.setState({ croppedImageUrl });
+    }
+  }
+
+  getCroppedImg(image, pixelCrop, fileName) {
+    const canvas = document.createElement("canvas");
+    canvas.width = pixelCrop.width;
+    canvas.height = pixelCrop.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      image,
+      pixelCrop.x,
+      pixelCrop.y,
+      pixelCrop.width,
+      pixelCrop.height,
+      0,
+      0,
+      pixelCrop.width,
+      pixelCrop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error("Canvas is empty");
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, "image/jpeg");
+    });
+  }
+
   addProfile = () => {
     const data = {};
     console.log(data);
-  }
+  };
 
   render() {
+    const { crop, croppedImageUrl, src } = this.state;
     return (
       <div>
         <Header />
@@ -236,6 +333,71 @@ export default class InstructorRegister extends Component {
                         &nbsp;&nbsp;&nbsp; Car hire for test
                       </label>
                     </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sectionBlocksQuestion">
+                <div className="sectionBlockHeader">
+                  <h4>Your Vehicle Images:</h4>
+                </div>
+                <div className="sectionBlockBody">
+                  <h5>Upload up to 3 images:</h5>
+                  <div className="formLinks">
+                    <div className="reactCropContainer">
+                      <div className="cropUpload">
+                        <input
+                          type="file"
+                          onChange={this.onSelectFile}
+                          className="custom-file-input"
+                        />
+                      </div>
+                      {src && (
+                        <ReactCrop
+                          src={src}
+                          crop={crop}
+                          onImageLoaded={this.onImageLoaded}
+                          onComplete={this.onCropComplete}
+                          onChange={this.onCropChange}
+                        />
+                      )}
+                      {croppedImageUrl && (
+                        <div className="confirmImageContainer">
+                          {/* <img
+                            alt="Crop"
+                            style={{ maxWidth: "100%" }}
+                            src={croppedImageUrl}
+                          /> */}
+                          <a onClick={this.confirmUpload}>Confirm Upload</a>
+                        </div>
+                      )}
+                      <div className="triImageContainer">
+                        <div>
+                          <img
+                            src={
+                              this.state.imagesrc1 ? this.state.imagesrc1 : ""
+                            }
+                          />
+                          <i class="far fa-image" />
+                        </div>
+                        <div>
+                          <img
+                            src={
+                              this.state.imagesrc2 ? this.state.imagesrc2 : ""
+                            }
+                          />
+                          <i class="far fa-image" />
+                        </div>
+                        <div>
+                          <img
+                            src={
+                              this.state.imagesrc3 ? this.state.imagesrc3 : ""
+                            }
+                          />
+                          <i class="far fa-image" />
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
